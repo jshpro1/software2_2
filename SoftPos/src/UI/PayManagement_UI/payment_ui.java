c93c95d6d0aaed8fe3870c2721489a32bde27952
realize2578 <realize2578@gmail.com>
2023-05-15 00:48:08 +0900

    [test] git test
    test

---
+++ SoftPos/src/UI/PayManagement_UI/payment_ui.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI.PayManagement_UI;
+
+/**
+ *
+ * @author ³²Áø¿ì
+ */
+public class payment_ui {
+    
+}
