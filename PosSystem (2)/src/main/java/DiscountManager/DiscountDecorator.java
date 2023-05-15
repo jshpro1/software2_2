3078c523d15cc71bbdcbd0ce0f8d8afcdd02408b
jsh <jshpro1@gmail.com>
2023-05-15 23:00:14 +0900

    Create DiscountDecorator.java

---
+++ PosSystem (2)/src/main/java/DiscountManager/DiscountDecorator.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DiscountManager;
+
+/**
+ *
+ * @author jsh
+ */
+
+//데코레이터 클래스 생성 
+
+public abstract class DiscountDecorator implements Discount {
+    private final Discount discount;
+
+    public DiscountDecorator(Discount discount) {
+        this.discount = discount;
+    }
+
+    @Override
+    public double applyDiscount(double price) {
+        return discount.applyDiscount(price);
+    }
+}
+
