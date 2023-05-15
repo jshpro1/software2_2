d47a653e9b6977b81f8a698e23aca4e17a52c382
jsh <jshpro1@gmail.com>
2023-05-15 23:00:09 +0900

    Create BaseDiscount.java

---
+++ PosSystem (2)/src/main/java/DiscountManager/BaseDiscount.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DiscountManager;
+
+/**
+ *
+ * @author jsh
+ */
+
+
+//할인 적용 업는 클래스
+public class BaseDiscount implements Discount {
+
+    @Override
+    public double applyDiscount(double price) {
+        return price;
+    }
+}
