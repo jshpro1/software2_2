78de5e24b10133b2f5b1532957ac1a27843b6e8c
jsh <jshpro1@gmail.com>
2023-05-21 23:25:54 +0900

    Create Main.java

---
+++ PosSystem (2)/src/main/java/login/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package login;
+
+/**
+ *
+ * @author jsh
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		new LoginScreen();
+
+	}
+
+}
